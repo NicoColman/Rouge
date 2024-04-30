ea4caec15dba0cac8c21e48828e346ff0b2f0f8f
NicoColman <nicolman01@gmail.com>
2024-04-30 15:23:36 -0300

    EquipStart

--- Source/Rouge/Private/GASManager/GameplayAbilities/PlayerAbilities/PlayerEquipAbility.cpp
+++ Source/Rouge/Private/GASManager/GameplayAbilities/PlayerAbilities/PlayerEquipAbility.cpp
@@ -3,3 +3,45 @@
 
 #include "GASManager/GameplayAbilities/PlayerAbilities/PlayerEquipAbility.h"
 
+#include "AbilitySystemComponent.h"
+#include "GlobalManagers/RougeGameplayTags.h"
+
+void UPlayerEquipAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                          const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                          const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	const UAbilitySystemComponent* AbilitySystemComponent = GetAbilitySystemComponentFromActorInfo();
+	if (!AbilitySystemComponent->HasMatchingGameplayTag(FRougeGameplayTags::Get().StateTag_Player_AbleEquip))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+	}
+	if (ActorInfo->AvatarActor.Get()->HasAuthority())
+	{
+		
+	}
+}
+
+AEffectActorBase* UPlayerEquipAbility::GetEffectActorFromActiveEffects(
+	UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTag& EffectTag)
+{
+	FGameplayEffectQuery Query;
+	Query.EffectTagQuery = FGameplayTagQuery::MakeQuery_MatchTag(EffectTag);
+	TArray<FActiveGameplayEffectHandle> ActiveEffectHandles = AbilitySystemComponent->GetActiveEffects(Query);
+
+	for (const FActiveGameplayEffectHandle& EffectHandle : ActiveEffectHandles)
+	{
+		const FActiveGameplayEffect* ActiveEffect = AbilitySystemComponent->GetActiveGameplayEffect(EffectHandle);
+		if (ActiveEffect && ActiveEffect->Spec.GetEffectContext().GetInstigator())
+		{
+			UObject* SourceObject = ActiveEffect->Spec.GetEffectContext().GetSourceObject();
+			AEffectActorBase* EffectActor = Cast<AEffectActorBase>(SourceObject);
+			if (EffectActor)
+			{
+				return EffectActor;
+			}
+		}
+	}
+	return nullptr;
+}
--- Source/Rouge/Public/GASManager/GameplayAbilities/PlayerAbilities/PlayerEquipAbility.h
+++ Source/Rouge/Public/GASManager/GameplayAbilities/PlayerAbilities/PlayerEquipAbility.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GASManager/EffectActors/EffectActorBase.h"
 #include "GASManager/GameplayAbilities/InputBaseAbility.h"
 #include "PlayerEquipAbility.generated.h"
 
@@ -13,5 +14,11 @@ UCLASS()
 class ROUGE_API UPlayerEquipAbility : public UInputBaseAbility
 {
 	GENERATED_BODY()
-	
+
+public:
+    virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+private:
+	AEffectActorBase* UPlayerEquipAbility::GetEffectActorFromActiveEffects(
+		UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTag& EffectTag);
 };
